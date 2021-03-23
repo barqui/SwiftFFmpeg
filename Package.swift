ba787cb1d7544242b163da2c4e5c807eb0ae84e4
seulbeom <seulbeom.kim@cupix.com>
2021-03-23 15:25:17 +0900

    update checksum

--- Package.swift
+++ Package.swift
@@ -46,41 +46,41 @@ let package = Package(
     .binaryTarget(
       name: "avcodec",
       url: "https://github.com/barqui/SwiftFFmpeg/blob/master/xcframework/avcodec.zip?raw=true",
-      checksum: "97f406b33946c1ab941af7dd02991e5cc3703779bb018de8ff265eb7a6d236fa"
+      checksum: "6a1af9fda24dbc5bd70a40751c3c14a735158e2fbc819e77c4be6365290e187f"
     ),
     .binaryTarget(
       name: "avdevice",
       url:
         "https://github.com/barqui/SwiftFFmpeg/blob/master/xcframework/avdevice.zip?raw=true",
-      checksum: "87738dff91380bd23de288e8702f0aa5511d80895c0974daba13dc117bdd599c"
+      checksum: "bbe7b60598bc042cbbb4175e6b9094d38b45ff0112944f9382e2c195c8b45668"
     ),
     .binaryTarget(
       name: "avfilter",
       url:
         "https://github.com/barqui/SwiftFFmpeg/blob/master/xcframework/avfilter.zip?raw=true",
-      checksum: "cb9b72b3cd923c9b065041cf6a87e164264f16eb282ea338b15bdae6dd5a3d15"
+      checksum: "024ea1fd7bde742976ef622ca9929348a0995e675156b5bf55b02047c4a1c278"
     ),
     .binaryTarget(
       name: "avformat",
       url:
         "https://github.com/barqui/SwiftFFmpeg/blob/master/xcframework/avformat.zip?raw=true",
-      checksum: "9def922d249207ed7be7aebdab7bbd2ff3bc41187dcb0bd77c8e216630407e00"
+      checksum: "ce9e702af7d69edca0e75d981917ca8f12b5aeff8b309cb5261ecf295c528043"
     ),
     .binaryTarget(
       name: "avutil",
       url: "https://github.com/barqui/SwiftFFmpeg/blob/master/xcframework/avutil.zip?raw=true",
-      checksum: "3ca05b51466e4fec332b5c1c607b228a67f4eb7a89e09944d19550474214f4e2"
+      checksum: "86802134cb419806728445da5c09688361c22269d89274832f01d62687ae1ca2"
     ),
     .binaryTarget(
       name: "swresample",
       url:
         "https://github.com/barqui/SwiftFFmpeg/blob/master/xcframework/swresample.zip?raw=true",
-      checksum: "76aa9805c67a040a729eee4599162a982f4bbd2609e7c92b7a3c589159259a7a"
+      checksum: "e69b84deab30aabf4c94122c03aef1e3281178922423129a0b1e21ee9d6804a0"
     ),
     .binaryTarget(
       name: "swscale",
       url: "https://github.com/barqui/SwiftFFmpeg/blob/master/xcframework/swscale.zip?raw=true",
-      checksum: "b5e47c27f44c6818cc11a41fe43535e7bdb25d171a0b5c4984b52b303c7e53b1"
+      checksum: "87f1f7627c558201ee9879bb709d12c0bfe929d29de26e27d0b92acca688d125"
     ),
     .target(
       name: "SwiftFFmpegExamples",
